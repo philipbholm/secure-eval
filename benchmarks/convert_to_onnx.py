cacf72086f6fec99d18b3c3f6ac8bea97745be97
Philip Holm <philipbaltzersen@gmail.com>
2025-03-07 11:59:48 +0100

    Change GlobalAveragePool to AveragePool

--- benchmarks/convert_to_onnx.py
+++ benchmarks/convert_to_onnx.py
@@ -4,7 +4,7 @@ import tf2onnx
 
 path = "data/ECG"
 keras_model_name = "model.keras"
-onnx_model_name = "ecg_classifier.onnx"
+onnx_model_name = "ecg_classifier"
 
 keras_model = tf.keras.models.load_model(f"{path}/{keras_model_name}")
 
@@ -12,23 +12,22 @@ keras_model = tf.keras.models.load_model(f"{path}/{keras_model_name}")
 spec = (tf.TensorSpec((None, 1000, 12), tf.float32, name="input"),)
 onnx_model, _ = tf2onnx.convert.from_keras(keras_model, input_signature=spec, opset=14)
 
-# Change GlobalAveragePool node to ReduceMean
+# Save model before processing
+onnx.save(onnx_model, f"{path}/{onnx_model_name}_base.onnx")
+
+# Change GlobalAveragePool node to AveragePool
 # since it is not supported by Concrete ML
-# GlobalAveragePool is a special case of ReduceMean, but the conversion
-# below might not be correct
-# TODO: Double check conversion
 for i, node in enumerate(onnx_model.graph.node):
     if node.op_type == "GlobalAveragePool":
         input_name = node.input[0]
         output_name = node.output[0]
-        reducemean_node = onnx.helper.make_node(
-            "ReduceMean",
+        new_node = onnx.helper.make_node(
+            "AveragePool",
             inputs=[input_name],
             outputs=[output_name],
-            axes=[2],
-            keepdims=1,
+            kernel_shape=[1000],
         )
         onnx_model.graph.node.remove(node)
-        onnx_model.graph.node.insert(i, reducemean_node)
+        onnx_model.graph.node.insert(i, new_node)
 
-onnx.save(onnx_model, f"{path}/{onnx_model_name}")
+onnx.save(onnx_model, f"{path}/{onnx_model_name}.onnx")
