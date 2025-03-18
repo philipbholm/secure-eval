9164b8782699dba349bd1417fb8e10e0ed41bee9
Philip Holm <philipbaltzersen@gmail.com>
2025-03-18 13:44:45 +0100

    Encrypt sample model

--- benchmarks/sample.py
+++ benchmarks/sample.py
@@ -7,13 +7,23 @@
 
 import crypten
 import torch
-from torchvision import datasets, transforms 
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
 
 crypten.init()
 torch.set_num_threads(1)
 
-mnist = datasets.MNIST("/tmp", download=True, train=True, transform=transforms.ToTensor())
-mean, std = mnist.data.float().mean().unsqueeze(0), mnist.data.float().std().unsqueeze(0)
+ALICE = 0
+BOB = 1
+
+mnist = datasets.MNIST(
+    "/tmp", download=True, train=True, transform=transforms.ToTensor()
+)
+mean, std = (
+    mnist.data.float().mean().unsqueeze(0),
+    mnist.data.float().std().unsqueeze(0),
+)
 mnist_norm = transforms.functional.normalize(mnist.data.float(), mean, std)
 mnist_labels = mnist.targets
 
@@ -24,3 +34,51 @@ torch.save(data[:10], "/tmp/alice_train.pth")
 torch.save(data[10:], "/tmp/bob_test.pth")
 torch.save(labels[:10], "/tmp/alice_train_labels.pth")
 torch.save(labels[10:], "/tmp/bob_test_labels.pth")
+
+
+class AliceNet(nn.Module):
+    def __init__(self):
+        super(AliceNet, self).__init__()
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = F.relu(out)
+        out = self.fc2(out)
+        out = F.relu(out)
+        out = self.fc3(out)
+        return out
+
+
+crypten.common.serial.register_safe_class(AliceNet)
+
+
+def compute_accuracy(output, labels):
+    pred = output.argmax(1)
+    correct = pred.eq(labels)
+    correct_count = correct.sum(0, keepdim=True).float()
+    accuracy = correct_count.mul_(100.0 / output.size(0))
+    return accuracy
+
+
+# Load pre-trained model to Alice
+dummy_model = AliceNet()
+plaintext_model = torch.load("models/tutorial4_alice_model.pth")
+
+print(plaintext_model)
+
+# Encrypt the model from Alice:
+
+# 1. Create a dummy input with the same shape as the model input
+dummy_input = torch.empty((1, 784))
+
+# 2. Construct a CrypTen network with the trained model and dummy_input
+private_model = crypten.nn.from_pytorch(plaintext_model, dummy_input)
+
+# 3. Encrypt the CrypTen network with src=ALICE
+private_model.encrypt(src=ALICE)
+
+# Check that model is encrypted:
+print("Model successfully encrypted:", private_model.encrypted)
