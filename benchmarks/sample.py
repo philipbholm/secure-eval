455cb9aeb2833154bbbd335f0228d7a38d8e66bb
Philip Holm <philipbaltzersen@gmail.com>
2025-03-18 14:24:49 +0100

    Add encrypted inference from tutorial

--- benchmarks/sample.py
+++ benchmarks/sample.py
@@ -8,6 +8,8 @@
 import warnings
 
 import crypten
+import crypten.communicator as comm
+import crypten.mpc as mpc
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -89,3 +91,36 @@ private_model.encrypt(src=ALICE)
 
 # Check that model is encrypted:
 print("Model successfully encrypted:", private_model.encrypted)
+
+
+# Classify encrypted data with encrypted model
+labels = torch.load("/tmp/bob_test_labels.pth").long()
+count = 100  # For illustration purposes, we'll use only 100 samples for classification
+
+
+@mpc.run_multiprocess(world_size=2)
+def encrypt_model_and_data():
+    # Load pre-trained model to Alice
+    model = crypten.load_from_party("models/tutorial4_alice_model.pth", src=ALICE)
+
+    # Encrypt model from Alice
+    dummy_input = torch.empty((1, 784))
+    private_model = crypten.nn.from_pytorch(model, dummy_input)
+    private_model.encrypt(src=ALICE)
+
+    # Load data to Bob
+    data_enc = crypten.load_from_party("/tmp/bob_test.pth", src=BOB)
+    data_enc2 = data_enc[:count]
+    data_flatten = data_enc2.flatten(start_dim=1)
+
+    # Classify the encrypted data
+    private_model.eval()
+    output_enc = private_model(data_flatten)
+
+    # Compute the accuracy
+    output = output_enc.get_plain_text()
+    accuracy = compute_accuracy(output, labels[:count])
+    crypten.print("\tAccuracy: {0:.4f}".format(accuracy.item()))
+
+
+encrypt_model_and_data()
